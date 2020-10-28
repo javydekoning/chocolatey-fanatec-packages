63af8bc98e2c84cc8b830efa34e0e3210c563551
unknown <javydekoning@gmail.com>
2020-10-28 15:41:05 +0000

    Pushing 1.47

--- fanalab/tools/chocolateyInstall.ps1
+++ fanalab/tools/chocolateyInstall.ps1
@@ -2,12 +2,11 @@
 
 $packageName = 'fanalab'
 $url = 'https://forum.fanatec.com/uploads/832/9QPM5VG48L3N.zip'
-$checksum = '053C486B63A0A1F02AD564167DBC32CAE88BBAA41F0BF1F104D505BC599CA546'
+$checksum = '591668ddcce4b1fc198d3607711e82caa9383541dd977cab2e3e4131bc609a79'
 $filePath = "$toolsDir\fanalab.zip"
 $fileType = 'zip'
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$embedded_path = Get-Item "$toolsDir\*.$fileType"
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $downloadArgs = @{
@@ -15,7 +14,7 @@ $downloadArgs = @{
   fileFullPath = $filePath
   url          = $url
   checksum     = $checksum
-  #checksumType = 'sha256'
+  checksumType = 'sha256'
   options      = @{
     Headers = @{             
       Accept = '*/*'
@@ -24,23 +23,25 @@ $downloadArgs = @{
 }
 
 Get-ChocolateyWebFile @downloadArgs
+Get-ChocolateyUnzip -FileFullPath $filePath -Destination $toolsDir
+
+$fileTypeUnzipped = 'msi'
+$embedded_path = Get-Item "$toolsDir\*.$fileTypeUnzipped"
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = $fileTypeUnzipped
+  file           = $embedded_path
+  silentArgs     = '/qn'
+  validExitCodes = @(0)
+  softwareName   = $packageName
+}
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $embedded_path -ea 0
 
 
-# $packageArgs = @{
-#   packageName    = $packageName
-#   fileType       = $fileType
-#   file           = $embedded_path
-#   silentArgs     = '/VERYSILENT /TASKS=' + ($tasks -join ',')
-#   validExitCodes = @(0)
-#   softwareName   = $packageName
-# }
-# Install-ChocolateyInstallPackage @packageArgs
-# rm $embedded_path -ea 0
-
-# $packageName = $packageArgs.packageName
-# $installLocation = Get-AppInstallLocation $packageName
-# if (!$installLocation) { Write-Warning "Can't find $packageName install location"; return }
-# Write-Host "$packageName installed to '$installLocation'"
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation) { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
 
 # Register-Application "$installLocation\$packageName.exe"
 # Write-Host "$packageName registered as $packageName"
