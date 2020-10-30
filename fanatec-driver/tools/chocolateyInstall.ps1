a51c814460ff016add84de5eda3167bad58b4891
javydekoning <javydekoning+github@gmail.com>
2020-10-30 23:08:14 +0100

    Changing cert for beta and ga

--- fanatec-driver/tools/chocolateyInstall.ps1
+++ fanatec-driver/tools/chocolateyInstall.ps1
@@ -1,32 +1,32 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath    = "$toolsDir\fanatec_driver.msi"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsDir\fanatec_driver.msi"
 $packageName = 'fanatec-driver'
-$url         = 'https://fanatec.com/media/unknown/fe/df/ab/Fanatec_32_driver_346.msi'
-$url64       = 'https://fanatec.com/media/unknown/a8/ff/14/Fanatec_64_driver_346.msi'
+$url = 'https://fanatec.com/media/unknown/fe/df/ab/Fanatec_32_driver_346.msi'
+$url64 = 'https://fanatec.com/media/unknown/a8/ff/14/Fanatec_64_driver_346.msi'
 
 $downloadArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileFullPath  = $filePath
-  url           = $url
-  url64bit      = $url64
-  checksum      = '157acfc38c9e5306cdd58ccbdbd9350773de1a3adbf88403c3dbb5e58aacce38'
-  checksumType  = 'sha256'
-  checksum64    = '119F39DCA732AD2925FC3D124ECF0D07EC42BDD25350A08B52308090861690CD'
-  checksumType64= 'sha256'
-  options      = @{
-      Headers = @{             
-          Accept  = '*/*'
-      }
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = $filePath
+  url            = $url
+  url64bit       = $url64
+  checksum       = '157acfc38c9e5306cdd58ccbdbd9350773de1a3adbf88403c3dbb5e58aacce38'
+  checksumType   = 'sha256'
+  checksum64     = '119F39DCA732AD2925FC3D124ECF0D07EC42BDD25350A08B52308090861690CD'
+  checksumType64 = 'sha256'
+  options        = @{
+    Headers = @{             
+      Accept = '*/*'
+    }
   }
 }
 
 Get-ChocolateyWebFile @downloadArgs
 
-$computerName  = $Env:COMPUTERNAME
+$computerName = $Env:COMPUTERNAME
 $installerPath = $downloadArgs.fileFullPath
-$store         = New-Object Security.Cryptography.X509Certificates.X509Store("$computerName\TrustedPublisher", 'LocalMachine')
+$store = New-Object Security.Cryptography.X509Certificates.X509Store("$computerName\TrustedPublisher", 'LocalMachine')
 
 $store.Open('ReadWrite, OpenExistingOnly')
 $store.Add((Get-AuthenticodeSignature $installerPath).SignerCertificate)
@@ -64,6 +64,41 @@ gUoJty66iVZBJXS9AcxwSxQ0pkRi1NGOnj4EPy4xlHyzhAPGKRrwwxoS/MtyBUEB
 UXW6gAe542zhM9r2C9C+DGnNSTIzq2Y1KITYEN5Vap4D44UtWBpOOArAQ65S5eT2
 pOFnS5v2cOfRBCYHnkYk71/oKYsFwiaL90rS5FpdVFtOg3qbodH9VII4GlKKhBtU
 6UKDMoPXuLBKOfF0DJVpjziYTZLA6y9zahqP93N+ZZ+TLfzItao9
+-----END CERTIFICATE-----
+"@ | Out-File $tmpfile -Encoding utf8 
+Import-Certificate -filepath $tmpfile -CertStoreLocation 'Cert:\localmachine\TrustedPublisher\'
+
+$tmpfile = [system.io.path]::GetTempFileName()
+@"
+-----BEGIN CERTIFICATE-----
+MIIFETCCA/mgAwIBAgIQKGOPh5gDgut6tYlwv9eO5TANBgkqhkiG9w0BAQUFADCB
+tDELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDlZlcmlTaWduLCBJbmMuMR8wHQYDVQQL
+ExZWZXJpU2lnbiBUcnVzdCBOZXR3b3JrMTswOQYDVQQLEzJUZXJtcyBvZiB1c2Ug
+YXQgaHR0cHM6Ly93d3cudmVyaXNpZ24uY29tL3JwYSAoYykxMDEuMCwGA1UEAxMl
+VmVyaVNpZ24gQ2xhc3MgMyBDb2RlIFNpZ25pbmcgMjAxMCBDQTAeFw0xODA3MTYw
+MDAwMDBaFw0xOTA5MTYyMzU5NTlaMHMxCzAJBgNVBAYTAlBIMRUwEwYDVQQIDAxN
+ZXRybyBNYW5pbGExEzARBgNVBAcMClBhc2lnIENpdHkxGzAZBgNVBAoMEkRhdHJv
+bmljc29mdCwgSW5jLjEbMBkGA1UEAwwSRGF0cm9uaWNzb2Z0LCBJbmMuMIIBIjAN
+BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2zHpYvtLF4FAkzhCHGUYc+MuzdHX
+niiyi9H6heSaoo7aQV3NHPWNoCoVZv+VarRmiCIX3Ej3+do7fbvFz8ZUNrFc4TnJ
+vlBPO/IbzEDunv/dFK31AbIOSFdZr8o3aVEGdTCaFOEuMNRg/oUnDfo9XgwfJ1q0
+yop1YRRSmdyHUMaRwDDDNMNBLFR/Qe2zdbDvIp5LBSNbm1pG92Qx/QmpYmLyYjlH
+XI7jZbsqacqIkFKpw3LBpAKom7sRshUOzLS+A/KTLw9Zb53AGZo7TRGK/MM7PLuf
+VMVyPBI/jgeQcF5OPDBkcB4WozX4tzZC0TRhkRbrRY0wha4FLmNC78NNtQIDAQAB
+o4IBXTCCAVkwCQYDVR0TBAIwADAOBgNVHQ8BAf8EBAMCB4AwKwYDVR0fBCQwIjAg
+oB6gHIYaaHR0cDovL3NmLnN5bWNiLmNvbS9zZi5jcmwwYQYDVR0gBFowWDBWBgZn
+gQwBBAEwTDAjBggrBgEFBQcCARYXaHR0cHM6Ly9kLnN5bWNiLmNvbS9jcHMwJQYI
+KwYBBQUHAgIwGQwXaHR0cHM6Ly9kLnN5bWNiLmNvbS9ycGEwEwYDVR0lBAwwCgYI
+KwYBBQUHAwMwVwYIKwYBBQUHAQEESzBJMB8GCCsGAQUFBzABhhNodHRwOi8vc2Yu
+c3ltY2QuY29tMCYGCCsGAQUFBzAChhpodHRwOi8vc2Yuc3ltY2IuY29tL3NmLmNy
+dDAfBgNVHSMEGDAWgBTPmanqeyb0S8mOj9fwBSbv49KnnTAdBgNVHQ4EFgQUYSAR
+h5aKVOg59HW2vtRPzqBf+LwwDQYJKoZIhvcNAQEFBQADggEBANydOw6TAo29TeQk
+p5Dll7j5pLDygukhR653PUhuTX9vA9KGyOaUdsxrUDxymghbcBQavfAOA3zX7tZL
+2vxWYICCLDADdnpQzWfMgvFtst5ytWF44Bjnxy7QEc1YPubgKVygmLuDr6KjJMq8
+RFqzjAokt4zBTRihaWaIXxQdgXbrCjtgMQtR6k95ncRfLDSDJoQc/j1+gKNu2zjb
+sBURO4d+K7ilsojRKZ/gl8qmqRvS4vbu6Gfj3JetetCGT9obeWIp88MtqGr74/Wk
+FwaOs6kS3SiH83E9RYabzLoHp/yQNTOPI9TIA1IOumzkjnFCq+RHV6ZRITMwZAUo
+j32yPWk=
 -----END CERTIFICATE-----
 "@ | Out-File $tmpfile -Encoding utf8 
 Import-Certificate -filepath $tmpfile -CertStoreLocation 'Cert:\localmachine\TrustedPublisher\'
