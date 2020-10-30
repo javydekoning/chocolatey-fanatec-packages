0d1dc1298fdcbd6ca3b7b3efd0197dbdab179206
javydekoning <javydekoning+github@gmail.com>
2020-10-30 12:08:39 +0100

    Testing new install.ps1

--- fanatec-driver/tools/chocolateyInstall.ps1
+++ fanatec-driver/tools/chocolateyInstall.ps1
@@ -1,17 +1,33 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageArgs = @{
-  packageName   = 'fanatec-driver'
-  fileType      = 'msi'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsDir\fanatec_driver.msi"
+
+$downloadArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileFullPath = $filePath
   url           = 'https://fanatec.com/media/unknown/fe/df/ab/Fanatec_32_driver_346.msi'
   url64bit      = 'https://fanatec.com/media/unknown/a8/ff/14/Fanatec_64_driver_346.msi'
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1641)
-  softwareName  = 'fanatec-driver'
   checksum      = '157acfc38c9e5306cdd58ccbdbd9350773de1a3adbf88403c3dbb5e58aacce38'
   checksumType  = 'sha256'
   checksum64    = '119F39DCA732AD2925FC3D124ECF0D07EC42BDD25350A08B52308090861690CD'
   checksumType64= 'sha256'
+  options      = @{
+      Headers = @{             
+          Accept  = '*/*'
+          Referer = 'https://www.amd.com/en/support/chipsets/amd-socket-am4/a320'
+      }
+  }
 }
 
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @downloadArgs
+
+$computerName = $Env:COMPUTERNAME
+$installerPath = $downloadArgs.fileFullPath
+$store = New-Object Security.Cryptography.X509Certificates.X509Store(
+  "$computerName\TrustedPublisher", 'LocalMachine')
+$store.Open('ReadWrite, OpenExistingOnly')
+$store.Add((Get-AuthenticodeSignature $installerPath).SignerCertificate)
+$store.Close()
+
+Install-ChocolateyPackage -url $downloadArgs.fileFullPath -silentArgs = "/qn /norestart" -validExitCodes @(0, 3010, 1641) -filetype = 'msi' -UseOriginalLocation
