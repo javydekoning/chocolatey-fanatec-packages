4913bd1292b3e45d4d154014c79ec292e2c93d3a
javydekoning <javydekoning+github@gmail.com>
2021-09-01 11:17:29 +0000

    AU: 1 updated - evga-precision-x1 [skip ci] https://gist.github.com/84b7570be09beb04ffb3ba3cd6d35eda/d1cbd8ff72e355bce3d41dcc1545a06f99d721cd

--- evga-precision-x1/tools/chocolateyInstall.ps1
+++ evga-precision-x1/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $toolsDir    = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'evga-precision-x1'
 
-$url = 'https://fichiers.touslesdrivers.com/69473/EVGA_Precision_X1_1.2.4.0.zip'
+$url = 'https://fichiers.touslesdrivers.com/69606/EVGA_Precision_X1_1.2.5.0.zip'
 $FileName = 'evga-precision-x1.zip'
-$checksum = 'f87261bdb719076bffee8add6b97c997541d4004688acc57c926e2303bddc73d'
+$checksum = 'b7630ba7ba0561c4febb4ee82b8c941c3552f0153953ff52ec69895568ef583f'
 
 $UnzipLocation = Join-Path "$env:TMP" ([io.path]::GetFileNameWithoutExtension( $FileName ))
 
