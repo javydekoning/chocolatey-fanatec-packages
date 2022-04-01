4e5ad3439e0cfc106255accbe7afafd36d049edc
javydekoning <javydekoning+github@gmail.com>
2022-04-01 11:19:33 +0000

    AU: 1 updated - fanalab [skip ci] https://gist.github.com/84b7570be09beb04ffb3ba3cd6d35eda/37187818fe7a863b00b565de5219100e8ef522c8

--- fanalab/tools/chocolateyInstall.ps1
+++ fanalab/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'fanalab'
-$url = 'https://forum.fanatec.com/uploads/762/1JRGD58GZT9W.zip'
-$checksum = 'ac7b999886f61cf255acf85a43d3d440144de4ddee464d1f5743879fe3c494e6'
+$url = 'https://forum.fanatec.com/uploads/092/HJ3Q8ZYH7KW4.zip'
+$checksum = 'f8e9ebf8e39dc9a6f4e4136b7ccd6db37325c0241b0444595f8697cbc0822dbf'
 $filePath = "$toolsDir\fanalab.zip"
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
